c75fd9c502be15bf66ab01d9bcb2acb9fb2f27ec
Cristina Poncela Cubeiro <140309543+cristinaponcela@users.noreply.github.com>
2026-08-04 10:16:49 +0200

    chore: 1 mig

--- packages/storage/sqlite-node/src/sqlite/migrations.ts
+++ packages/storage/sqlite-node/src/sqlite/migrations.ts
@@ -19,11 +19,6 @@ export async function loadMigrations(): Promise<SqliteMigration[]> {
 			order: 1,
 			sql: await loadMigrationSql("./migrations/001_initial.sql"),
 		},
-		{
-			id: "002_lanes.sql",
-			order: 2,
-			sql: await loadMigrationSql("./migrations/002_lanes.sql"),
-		},
 	];
 }
 
